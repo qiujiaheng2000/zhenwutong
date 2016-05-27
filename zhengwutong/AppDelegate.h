763615f8ad0eb9f115656486d52f2f703bf8373b
qiujiaheng <qiujiaheng@gmail.com>
2016-05-27 22:52:04 +0800

    Initial Commit

---
+++ zhengwutong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhengwutong
+//
+//  Created by qiujiaheng on 16/5/27.
+//  Copyright © 2016年 leqimin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
